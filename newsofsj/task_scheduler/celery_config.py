6e9a23abe665c5f3e98ff26b7db5e4c6f97b6467
kamisato zhang <2362589239@bupt.edu.cn>
2024-12-08 23:19:21 +0800

    Co-authored-by: kamisato zhang <2362589239@bupt.edu.cn>

--- newsofsj/task_scheduler/celery_config.py
+++
@@ -1,7 +0,0 @@
-broker_url = 'pyamqp://guest@localhost//'
-result_backend = 'redis://localhost'
-task_serializer = 'json'
-result_serializer = 'json'
-accept_content = ['json']
-timezone = 'UTC'
-enable_utc = True
